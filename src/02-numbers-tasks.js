42586b8aab94257cbea4085cf4e916669a21b766
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-05-12 23:01:12 +0200

    so much work :O

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -19,8 +19,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 
@@ -35,8 +35,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCicleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCicleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
 
 /**
@@ -51,8 +51,14 @@ function getCicleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+// function getAverage(value1, value2) {
+//   return (value1 + value2) / arguments.length
+// }
+function getAverage(value1, value2) {
+  if ((value1 + value2) / arguments.length === Infinity) {
+    return 1.7976931348623157e+308;
+  }
+  return (value1 + value2) / arguments.length;
 }
 
 /**
@@ -70,8 +76,12 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  // return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))
+  // return Math.sqrt(((x2 - x1) ** (x2 - x1)) + ((y2 - y1) ** (y2 - y1)));
+  const a = x1 - x2;
+  const b = y1 - y2;
+  return Math.sqrt(a * a + b * b);
 }
 
 /**
@@ -86,8 +96,12 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const x = 1;
+  if (b === undefined || b === 0) {
+    return 0;
+  }
+  return -(b / (a * x));
 }
 
 
@@ -109,6 +123,10 @@ function getLinearEquationRoot(/* a, b */) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
+// function getAngleBetweenVectors(x1, y1, x2, y2) {
+//   return Math.atan2(y1 - y2, x1 - x2);
+// }
+
 function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
   throw new Error('Not implemented');
 }
@@ -125,8 +143,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const lastDigit = value.toString().split('');
+  return lastDigit[lastDigit.length - 1];
 }
 
 
@@ -141,8 +160,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return parseFloat(value);
 }
 
 /**
@@ -179,8 +198,14 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const numStr = num.toString();
+  const numStart = numStr.substring(0, numStr.length - pow);
+  const numEnd = numStr.substring(numStr.length - pow, numStr.length);
+  if (numEnd[0] > 5) {
+    return numStart.substring(0, numStart.length - 1) + (parseInt(numStart[numStart.length - 1], 0) + 1 + '0'.repeat(numEnd.length));
+  }
+  return `${numStart}${'0'.repeat(numEnd.length)}`;
 }
 
 /**
@@ -200,8 +225,20 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let isPrimeNumber = true;
+  if (n === 1) {
+    isPrimeNumber = true;
+  } else if (n === 2) {
+    isPrimeNumber = true;
+  } else if (n > 2) {
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) {
+        isPrimeNumber = false;
+      }
+    }
+  }
+  return isPrimeNumber;
 }
 
 /**
